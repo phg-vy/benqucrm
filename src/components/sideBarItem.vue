955285998921cd76ffebd229807d4e9bf61c81e1
DIEUHA <sunidieu@gmail.com>
2025-05-10 15:23:45 +0700

    test branch

--- src/components/sideBarItem.vue
+++ src/components/sideBarItem.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex p-3 rounded-xl gap-3 font-semibold text-gray-500 ">
         <img src="/icon/blogDefault.svg" alt="">
-        Blog
+        Blog 1
     </div>
 </template>
